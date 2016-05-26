ec6e5a1281560831b94fbf9c75fc37aacdd3df20
Jenny Nguyen <jenny.ngoc.lan@gmail.com>
2016-05-26 23:53:33 +1000

    Made the search items have nicer alignment of font. Call to API is still asynchronous, but a progress dialogue appears when fetching now.

--- app/src/main/java/com/id11236662/gokeigo/view/SearchFragment.java
+++ app/src/main/java/com/id11236662/gokeigo/view/SearchFragment.java
@@ -1,7 +1,11 @@
 package com.id11236662.gokeigo.view;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -25,6 +29,7 @@ import com.id11236662.gokeigo.util.ApiInterface;
 import com.id11236662.gokeigo.util.Constants;
 import com.id11236662.gokeigo.util.MenuTint;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,14 +38,12 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * This fragment displays a Recycler View list and maintains a Entry Manager that manages entries
- * in the application. Added a search action on to the toolbar and implemented query functionality
- * on to it. The search is a filter type.
+ * This fragment displays a Recycler View list.
+ * Has a search action on the toolbar, which has query functionality implemented on it.
  */
 public class SearchFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private RecyclerView mRecyclerView;
-    private SearchAdapter mAdapter;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -51,25 +54,26 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
     }
 
     /**
-     * @param inflater used to inflate views in this fragment
-     * @param container this fragment
+     * @param inflater           used to inflate views in this fragment
+     * @param container          this fragment
      * @param savedInstanceState unused argument
      * @return the view hierarchy associated with this fragment
      */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_search, container, false);
+
         // Initialise the recycler view field with the value before returning it
         mRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_search_recycler_view);
+
         // Add ItemDecoration to recycler view
         mRecyclerView.addItemDecoration(new SearchAdapter.DividerItemDecoration(getActivity()));
         return view;
     }
 
     /**
-     * @param view returned result of onCreateView
+     * @param view               returned result of onCreateView
      * @param savedInstanceState unused argument
      */
     @Override
@@ -81,33 +85,17 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         // Setup Recycler View
         FragmentActivity activity = getActivity();
         mRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
-
-        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<EntriesResponse> call = apiInterface.getEntries("miru");
-        call.enqueue(new Callback<EntriesResponse>() {
-            @Override
-            public void onResponse(Call<EntriesResponse> call, Response<EntriesResponse> response) {
-                List<Entry> entries = response.body().getData();
-                mAdapter = new SearchAdapter(entries, R.layout.item_search);
-                mRecyclerView.setAdapter(mAdapter);
-                Log.d(Constants.TAG, "Number of entries received: " + entries.size());
-            }
-
-            @Override
-            public void onFailure(Call<EntriesResponse> call, Throwable t) {
-                // Log error here since request failed
-                Log.e(Constants.TAG, t.toString());
-            }
-        });
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate the menu; this adds items to the action bar if it is present
         inflater.inflate(R.menu.main, menu);
+
         // Manually tint the search icon as it's provided separately from AppCompat
         final MenuItem item = menu.findItem(R.id.action_search);
         MenuTint.colorMenuItem(item, Color.WHITE, null);
+
         // Set QueryTextListener on search view
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
         searchView.setOnQueryTextListener(this);
@@ -118,47 +106,78 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
      *
      * @param query the new content of the query text field.
      * @return false if the SearchView should perform the default action of showing any
-     * suggestions if available, true if the action was handled by the listener.
+     * suggestions if available, true if the action was handled by the listener. TODO: revise this
      */
     @Override
     public boolean onQueryTextChange(String query) {
-        // TODO: Make async call when fetching information
-//        final List<Entry> filteredWordList = filter(mEntryManager.getEntries(), query);
-//        mAdapter.animateTo(filteredWordList);
-//        // Ensure user can always see all items when searching for something
-//        mRecyclerView.scrollToPosition(0);
-//        return true;
+        // TODO: Make async call when fetching suggestions somehow
         return false;
     }
 
     /**
-     * Called when the query text is submitted by the user. Does nothing special though.
+     * Called when the query text is submitted by the user.
+     * Call the API to search up possible entries with the query text.
+     *
      * @param query the new content of the query text field.
-     * @return false, always.
+     * @return true, always.
      */
     @Override
     public boolean onQueryTextSubmit(String query) {
-        // TODO: Make async call when fetching information
-        return false;
+        new SearchJishoAsyncTask().execute(query);
+        return true;
     }
 
-    /**
-     * Filters entries with the provided query string
-     * @param entries list of entries to filter through
-     * @param query the string to match with any part of the kanji/kana or romaji of every entry
-     * @return a list of entries that have been filtered
-     */
-    private static List<Entry> filter(List<Entry> entries, String query) {
-        query = query.toLowerCase();
+    private class SearchJishoAsyncTask extends AsyncTask<String, Void, List<Entry>> {
 
-        final List<Entry> filteredEntryList = new ArrayList<>();
-//        for (Entry word : entries) {
-//            final String kanjiKana = word.getWordInKanjiKana();
-//            final String romaji = word.getWordInLowerCaseRomaji();
-//            if (kanjiKana.contains(query) || romaji.contains(query)) {
-//                filteredEntryList.add(word);
-//            }
-//        }
-        return filteredEntryList;
+        private ProgressDialog mProgressDialog;
+
+        /**
+         * Before executing new thread, show dialogue that the search process will soon execute.
+         */
+        @Override
+        protected void onPreExecute() {
+            mProgressDialog = new ProgressDialog(getContext());
+            mProgressDialog.setMessage(getString(R.string.message_searching) + "...");
+            mProgressDialog.show();
+        }
+
+        /**
+         * Call the API for a list of entries by supplying a keyword for the URI query.
+         * Execute search process.
+         *
+         * @param params Keyword for the URI query.
+         * @return list of entries retrieved from the API.
+         */
+        @Override
+        protected List<Entry> doInBackground(String... params) {
+            String keyword = params[0];
+            // TODO: if not connected to the network...
+            ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+            Call<EntriesResponse> call = apiInterface.getEntries(keyword);
+            try {
+                return call.execute().body().getData();
+            } catch (IOException e) {
+                Log.e(Constants.TAG, e.toString());
+            }
+            return null;
+        }
+
+        /**
+         * Dismiss the progress dialogue after execution and set the adapter on to recycler view.
+         *
+         * @param entries list of entries returned by doInBackground()
+         */
+        @Override
+        protected void onPostExecute(List<Entry> entries) {
+            if (mProgressDialog.isShowing()) {
+                mProgressDialog.dismiss();
+            }
+            if (entries != null) {
+                SearchAdapter adapter = new SearchAdapter(entries, R.layout.item_search);
+                mRecyclerView.setAdapter(adapter);
+            } else {
+                // TODO: show message that no results were found
+            }
+        }
     }
 }
