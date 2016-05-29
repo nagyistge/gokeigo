7ecdf6747578196809f34616859925354fa7b49c
Jenny Nguyen <jenny.ngoc.lan@gmail.com>
2016-05-29 23:50:30 +1000

    Revised comments.

--- app/src/main/java/com/id11236662/gokeigo/util/StringUtils.java
+++ app/src/main/java/com/id11236662/gokeigo/util/StringUtils.java
@@ -7,12 +7,16 @@ import java.util.List;
  */
 public class StringUtils {
 
+    /**
+     * @param strings a List of strings to be concatenated together.
+     * @return the list of strings in one concatenated string with a comma (,) delimiter
+     */
     public static String join(List<String> strings) {
         String result = "";
         for (int i = 0; i < strings.size(); i++) {
             result += strings.get(i);
 
-            // if not the last element, add a comma
+            // Add comma to the list if it's not the last element.
             if (i < strings.size() -1) {
                 result += ", ";
             }
