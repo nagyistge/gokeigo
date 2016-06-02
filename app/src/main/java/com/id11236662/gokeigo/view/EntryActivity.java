8ebce375e973e9726d7fa97b1b4a337c5aa2ca61
Jenny Nguyen <jenny.ngoc.lan@gmail.com>
2016-06-03 04:04:00 +1000

    Search results and entry screen looking better. Fixed the note not persisting bug. I forgot to set the mEntry with the previouslySavedEntry if it isn't null!

--- app/src/main/java/com/id11236662/gokeigo/view/EntryActivity.java
+++ app/src/main/java/com/id11236662/gokeigo/view/EntryActivity.java
@@ -1,6 +1,5 @@
 package com.id11236662.gokeigo.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
@@ -9,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.id11236662.gokeigo.R;
@@ -16,13 +16,15 @@ import com.id11236662.gokeigo.model.Entry;
 import com.id11236662.gokeigo.model.EntryManager;
 import com.id11236662.gokeigo.util.ActivityConfigurator;
 import com.id11236662.gokeigo.util.Constants;
+import com.raizlabs.android.dbflow.StringUtils;
 
 public class EntryActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EntryManager mEntryManager = EntryManager.getInstance();
     private Entry mEntry;
-    static final int SAVE_ENTRY_NOTE = 1;
-    private TextView mNotesTextView;
+    private boolean mInEditMode = false;
+    private FloatingActionButton mFab;
+    private EditText mNotesEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,18 +50,25 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
             Entry previouslySavedEntry = mEntryManager.getEntry(selectedEntry);
 
             if (previouslySavedEntry != null) {
+                // Save the entry for later use in the rest of the class.
+
+                mEntry = previouslySavedEntry;
+
+
                 // TODO: Update history date
+
+
             } else {
+
+                // Save the entry for later use in the rest of the class.
+
+                mEntry = selectedEntry;
 
                 // Save selected entry to history for the first time.
                 // TODO: If clear history, clear the dates of every entry. Only delete entries if no notes nor hasn't been starred.
 
                 mEntryManager.insertEntry(selectedEntry);
             }
-
-            // Save the entry for later use in the rest of the class.
-
-            mEntry = selectedEntry;
 
             // Set values to all the text views with the parcelled entry!
 
@@ -68,17 +77,28 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
             TextView commonTextView = (TextView) findViewById(R.id.activity_entry_common_status_text_view);
             TextView blurbTextView = (TextView) findViewById(R.id.activity_entry_blurb_text_view);
             TextView otherFormsTextView = (TextView) findViewById(R.id.activity_entry_other_forms_text_view);
-            mNotesTextView = (TextView) findViewById(R.id.activity_entry_notes_text_view);
+            mNotesEditText = (EditText) findViewById(R.id.activity_entry_notes_edit_text);
             assert wordTextView != null;
             wordTextView.setText(mEntry.getWord());
             assert readingTextView != null;
             readingTextView.setText(mEntry.getReading());
             assert blurbTextView != null;
             blurbTextView.setText(mEntry.getBlurb());
+            assert mNotesEditText != null;
+            mNotesEditText.setText(mEntry.getNotes());
+
+            // Show other forms if there is any, hide related views if there isn't any.
+
             assert otherFormsTextView != null;
-            otherFormsTextView.setText(mEntry.getOtherForms());
-            assert mNotesTextView != null;
-            mNotesTextView.setText(mEntry.getNotes());
+            String otherForms = mEntry.getOtherForms();
+            if (StringUtils.isNotNullOrEmpty(otherForms)) {
+                otherFormsTextView.setText(otherForms);
+            } else {
+                otherFormsTextView.setVisibility(View.GONE);
+                TextView otherFormsLabelTextView = (TextView) findViewById(R.id.activity_entry_other_forms_label_text_view);
+                assert otherFormsLabelTextView != null;
+                otherFormsLabelTextView.setVisibility(View.GONE);
+            }
 
             // Show common text view if is common, hide if not common.
 
@@ -96,9 +116,9 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
 
         // Initialise fab field and set click listener.
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.activity_entry_notes_fab);
-        assert fab != null;
-        fab.setOnClickListener(this);
+        mFab = (FloatingActionButton) findViewById(R.id.activity_entry_notes_fab);
+        assert mFab != null;
+        mFab.setOnClickListener(this);
     }
 
     /**
@@ -108,61 +128,29 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
      */
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.activity_entry_notes_fab) {
-            Intent intent = new Intent(this, NoteActivity.class);
-            intent.putExtra(Constants.INTENT_SELECTED_ENTRY, mEntry);
+        switch (v.getId()) {
+            case R.id.activity_entry_notes_fab:
 
-            Log.d(Constants.TAG_DEBUGGING, "EntryActivity.onClick. notes: " + mEntry.getNotes());
+                // TODO: share the entry. include ankidroid option
 
-            startActivityForResult(intent, SAVE_ENTRY_NOTE);
-        }
-    }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //outState
-        // TODO: use note fragment again? i don't want this activity to be destroyed.
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-
-    /**
-     * This is called if we get a result back from NoteActivity
-     *
-     * @param requestCode
-     * @param resultCode  unused parameter
-     * @param data        unused parameter
-     */
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // Check if the requestCode corresponds to what we asked for.
-
-        switch (requestCode) {
-            case SAVE_ENTRY_NOTE:
-
-                // Get the resulted note, set it to the entry object, update it in the DB and reset
-                // the text on display.
-
-                String notes = data.getExtras().getString(Constants.INTENT_ENTRY_NOTE);
-
-                Log.d(Constants.TAG_DEBUGGING, "EntryActivity.onActivityResult. notes: " + notes);
-
-                mEntry.setNotes(notes);
-                mEntryManager.updateEntry(mEntry);
-                mNotesTextView.setText(notes);
                 break;
         }
     }
 
     @Override
+    public void onBackPressed() {
+        // Update mEntry object with the note and save it to the DB, before going back Home.
+
+        String notes = mNotesEditText.getText().toString();
+        mEntry.setNotes(notes);
+        mEntryManager.updateEntry(mEntry);
+
+        super.onBackPressed();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO: what happens if it was the NoteActivity that called?
         switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
